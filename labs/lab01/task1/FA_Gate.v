9b809e1bb9dfd01acaf8bea195132456380f74b1
Pushkar Kumar <f20250657@pilani.bits-pilani.ac.in>
2026-08-29 11:36:14 +0000

    lab01 task1

--- labs/lab01/task1/FA_Gate.v
+++ labs/lab01/task1/FA_Gate.v
@@ -16,11 +16,11 @@ module FA_Gate(
 );
   wire ps, pc1, pc2;
 
- or  #(2) (cout, pc1, pc2);
-  xor #(2) (sum,  cin, ps);
-  and #(2) (pc1,  a,   b);
-  and #(2) (pc2,  cin, ps);
-  xor #(2) (ps,   a,   b);
+ or  #(2,3) (cout, pc1, pc2);
+  xor #(2,3) (sum,  cin, ps);
+  and #(2,3) (pc1,  a,   b);
+  and #(2,3) (pc2,  cin, ps);
+  xor #(2,3) (ps,   a,   b);
 
   
 
