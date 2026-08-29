d6c888d69808b4efd49efffcc5e0eacf5d1765d6
Pushkar Kumar <f20250657@pilani.bits-pilani.ac.in>
2026-08-29 10:28:13 +0000

    lab01 task2

--- labs/lab01/task2/FA_Gate.v
+++ labs/lab01/task2/FA_Gate.v
@@ -30,6 +30,6 @@ module FA_Gate(
   and #(2,3) (pc1,  a,   b);
   xor #(2,3) (sum,  cin, ps);
   and #(2,3) (pc2,  cin, ps);
-  or  #(2,3) (cout, pc1, pc2);
+  or #(2,3) (cout, pc1, pc2);
 
 endmodule
